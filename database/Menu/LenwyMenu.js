b40aae0798277815b6dcf336134fab9a71db8b7e
rezyganteng1326-debug <rezyganteng1326@gmail.com>
2026-08-07 16:38:11 +0700

    Delete database/Menu/LenwyMenu.js

--- database/Menu/LenwyMenu.js
+++
@@ -1,7 +0,0 @@
-global.lenwymenu = `*Menu Bot Lenwy*
-
-*> !Ai - Tanya Jawab AI*
-*> !Igdl - Download video Instagram*
-*> !Ttdl - Download video TikTok*
-*> !Tebakangka - Game Tebak angka*
-*> !Quote - Dapatkan Motivasi Hari Ini*
