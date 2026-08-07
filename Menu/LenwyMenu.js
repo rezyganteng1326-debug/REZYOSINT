ef670617acc5a54d0320a1b61346c656f1b920b6
rezyganteng1326-debug <rezyganteng1326@gmail.com>
2026-08-07 16:06:21 +0700

    Create LenwyMenu.js

---
+++ Menu/LenwyMenu.js
@@ -0,0 +1,7 @@
+global.lenwymenu = `*Menu Bot Lenwy*
+
+*> !Ai - Tanya Jawab AI*
+*> !Igdl - Download video Instagram*
+*> !Ttdl - Download video TikTok*
+*> !Tebakangka - Game Tebak angka*
+*> !Quote - Dapatkan Motivasi Hari Ini*
